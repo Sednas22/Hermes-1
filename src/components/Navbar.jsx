ee6e04900c5a14e0119eea8d94d49cfd44c79549
Cainã Sandes <caina.sb2018@gmail.com>
2026-06-05 19:52:41 -0300

    responsividade e sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
+  const [aberto, setAberto] = useState(false)
+
   return (
     <nav className="navbar">
       <div className="container">
@@ -8,12 +11,33 @@ export default function Navbar() {
           <div className="navbar-dot" />
           <span>HERMES-1</span>
         </div>
+
         <ul className="navbar-links">
-          <li><NavLink to="/" end>Dashboard</NavLink></li>
-          <li><NavLink to="/historico">Histórico</NavLink></li>
-          <li><NavLink to="/tecnologia">Tecnologia</NavLink></li>
-          <li><NavLink to="/sustentabilidade">Sustentabilidade</NavLink></li>
-          <li><NavLink to="/tripulacao">Tripulação</NavLink></li>
+          <li><NavLink to="/" end onClick={() => setAberto(false)}>Dashboard</NavLink></li>
+          <li><NavLink to="/historico" onClick={() => setAberto(false)}>Histórico</NavLink></li>
+          <li><NavLink to="/tecnologia" onClick={() => setAberto(false)}>Tecnologia</NavLink></li>
+          <li><NavLink to="/sustentabilidade" onClick={() => setAberto(false)}>Sustentabilidade</NavLink></li>
+          <li><NavLink to="/tripulacao" onClick={() => setAberto(false)}>Tripulação</NavLink></li>
+        </ul>
+
+        <button
+          className={`hamburguer ${aberto ? 'ativo' : ''}`}
+          onClick={() => setAberto(!aberto)}
+          aria-label="Abrir menu"
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+      </div>
+
+      <div className={`mobile-menu ${aberto ? 'aberto' : ''}`}>
+        <ul>
+          <li><NavLink to="/" end onClick={() => setAberto(false)}>Dashboard</NavLink></li>
+          <li><NavLink to="/historico" onClick={() => setAberto(false)}>Histórico</NavLink></li>
+          <li><NavLink to="/tecnologia" onClick={() => setAberto(false)}>Tecnologia</NavLink></li>
+          <li><NavLink to="/sustentabilidade" onClick={() => setAberto(false)}>Sustentabilidade</NavLink></li>
+          <li><NavLink to="/tripulacao" onClick={() => setAberto(false)}>Tripulação</NavLink></li>
         </ul>
       </div>
     </nav>
